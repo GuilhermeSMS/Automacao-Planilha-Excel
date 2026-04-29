0d238eb097f000a46f68ed9869de62e2f6516e58
GuilhermeSMS <guilherme.muniz2007@gmail.com>
2026-04-29 00:02:21 -0300

    Adicionando projeto de automacao

--- src/main/java/com/automacao/LeitorExcel.java
+++ src/main/java/com/automacao/LeitorExcel.java
@@ -17,32 +17,24 @@ public class LeitorExcel {
 
             Sheet sheet = workbook.getSheetAt(0); 
 
-            
             for (Row row : sheet) {
-                
                 Cell cell = row.getCell(0); 
-                
                 if (cell != null) {
-                    
                     String valorCelula = "";
                     if (cell.getCellType() == CellType.STRING) {
                         valorCelula = cell.getStringCellValue();
                     } else if (cell.getCellType() == CellType.NUMERIC) {
-                       
                         valorCelula = String.valueOf((long)cell.getNumericCellValue());
                     }
-                    
                     if (!valorCelula.isEmpty()) {
                         codigos.add(valorCelula.trim());
                     }
                 }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Erro ao ler o arquivo Excel: " + e.getMessage());
         }
-        
         System.out.println("Total de " + codigos.size() + " códigos lidos do Excel.");
         return codigos;
     }
