4c8f41938a5a96577625086e237200199051d2d9
Guilherme Muniz <guilherme.muniz2007@gmail.com>
2026-04-28 23:53:25 -0300

    Remove commented sections and clean up code

--- src/main/java/com/automacao/AutomacaoScan.java
+++ src/main/java/com/automacao/AutomacaoScan.java
@@ -16,17 +16,12 @@ public class AutomacaoScan {
 
     public static void main(String[] args) {
 
-        // --- 1. CONFIGURAÇÃO ---
-        
-        
         String CAMINHO_CHROMEDRIVER = "chromedriver.exe";
         String CAMINHO_PLANILHA = "automacao.xlsx";
         String URL_DO_SITE_FLOW = "https://www.amazonlogistics.com/sortcenter/m/containerization/flow?containerId=&stackingAreaId=";
 
-        // Configura o Selenium
         System.setProperty("webdriver.chrome.driver", CAMINHO_CHROMEDRIVER);
         WebDriver driver = new ChromeDriver();
-        // Cria uma "espera inteligente" de 10 segundos
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         Scanner leitorTerminal = new Scanner(System.in);
         System.out.println("Continuando automação...");
@@ -41,7 +36,6 @@ public class AutomacaoScan {
             driver.manage().window().maximize();
 
            
-            // [PAUSA PARA O LOGIN MANUAL COMPLETO]
             System.out.println("======================================================");
             System.out.println("--- SCRIPT PAUSADO ---");
             System.out.println("O navegador foi aberto.");
@@ -54,7 +48,6 @@ public class AutomacaoScan {
 
             while (true) {
             
-            // --- 3. LER O EXCEL ---
             LeitorExcel leitor = new LeitorExcel();
             
 
@@ -64,11 +57,9 @@ public class AutomacaoScan {
             
             for (String codigo : codigosParaEscanear) {
                 try {
-            // 1. Espera o campo de scan (sd_input) estar pronto
             wait.until(ExpectedConditions.elementToBeClickable(By.id(ID_CAMPO_SCAN)));
             WebElement campoScan = driver.findElement(By.id(ID_CAMPO_SCAN));
             
-            // 2. Digita o código e aperta Enter
             System.out.println("Enviando código: " + codigo);
             campoScan.clear();
             campoScan.sendKeys(codigo);
@@ -76,7 +67,7 @@ public class AutomacaoScan {
 
             
            
-            Thread.sleep(1000); // 1000 = 1 segundo. 
+            Thread.sleep(1000);
 
                 } catch (Exception e) {
                 }
@@ -107,13 +98,12 @@ public class AutomacaoScan {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // --- 5. FINALIZAR ---
             System.out.println("======================================================");
             System.out.println("SCAN FINALIZADO!");
             System.out.println("Total de pacotes na planilha: " + codigosParaEscanear.size());
             System.out.println("======================================================");
             try {
-        Thread.sleep(20000); // Espera 10s para você ver o resultado
+        Thread.sleep(20000);
 }       catch (InterruptedException e) {
     
         e.printStackTrace();
